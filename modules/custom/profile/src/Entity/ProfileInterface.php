05ac30011f63efddcb4af8b670b558d4593b2bde
Baher Rais <baher.rais@gmail.com>
2017-09-15 18:09:52 +0200

    Module Using Entity and Entity type on Drupal 8

---
+++ modules/custom/profile/src/Entity/ProfileInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\profile\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/*
+    Creation of the Profile entity interface
+*/
+interface ProfileInterface extends ContentEntityInterface{
+
+}
